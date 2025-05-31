a597bc7ab53e682b3facfcfb50114ae12199301d
cvkchris <chris11.vinod@gmail.com>
2025-05-31 09:40:53 +0530

    fixed a bug

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_URL,
+    host: process.env.HOST,
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
