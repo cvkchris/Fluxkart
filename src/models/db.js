1fbc8d2093a135de0e98da0f624bed4f37f73a67
cvkchris <chris11.vinod@gmail.com>
2025-05-31 09:30:05 +0530

    Fix database connection pool configuration by increasing timeout

--- src/models/db.js
+++ src/models/db.js
@@ -2,13 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
+    host: process.env.DB_URL,
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
+    connectTimeout: 10000,
     queueLimit: 0
 });
 
